277a1f7a2ac206de86575cb83b3561bbef1176ae
OmerYes <48590724+OmerYes@users.noreply.github.com>
2020-11-06 17:04:08 +0300

    --entity oluşturuldu

--- Ritmus.Data/Entity/Chart.cs
+++ Ritmus.Data/Entity/Chart.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ritmus.Data
+namespace Ritmus.Data.Entity
 {
-    public class Class1
+    class Chart
     {
     }
 }
